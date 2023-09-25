ab9cb1bdff871a2d1b08cab370226f08f0bb917a
SageM <madduxsage@gmail.com>
2023-09-25 11:27:15 -0400

    js code working  call complete

--- app.js
+++ app.js
@@ -3,18 +3,46 @@ let instructionsDiv = document.querySelector('#instructionsdiv')
 let searchAndButtonDiv = document.querySelector('#searchandbuttondiv')
 let glowingButton = document.querySelector('.glowing-btn')
 let inputSearch = document.querySelector('#inputsearch')
-const apiKey = '2568348056657128'
+const apiKey = '9fe371286bmsh0ed916646153ee4p1f72d4jsnb6ca301a19c9'
 
+
+
+ 
+
+
+
+console.log("working")
 
 
 //Event listener for click on glowing button.
 glowingButton.addEventListener('click', async () => {
-    let searchBarReturnValue = inputSearch.value
-    console.log(searchBarReturnValue)
 
-    //API call with the text from the search bar injected.
-    let response = await axios.get(`https://superheroapi.com/api/${apiKey}/search/${searchBarReturnValue}`)
-    console.log(response.data.response)
+    const options = {
+        method: 'GET',
+        url: 'https://superhero-search.p.rapidapi.com/api/heroes',
+        headers: {
+          'X-RapidAPI-Key': '9fe371286bmsh0ed916646153ee4p1f72d4jsnb6ca301a19c9',
+          'X-RapidAPI-Host': 'superhero-search.p.rapidapi.com'
+        }
+      };
+      
+      try {
+          const response = await axios.request(options);
+          console.log(response.data);
+      } catch (error) {
+          console.error(error);
+      }
+
+
+
+
+
+
+    
+
+    // //API call with the text from the search bar injected.
+    // let response = await axios.get(`https://superhero-search.p.rapidapi.com/api/${apiKey}/search/${searchBarReturnValue}`)
+    // console.log(response.data.response)
 
 
 
