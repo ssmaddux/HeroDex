58794a8a71bd77053ca3cab10bfd3a71304847db
SageM <madduxsage@gmail.com>
2023-09-25 14:17:06 -0400

    instructions&seachbardiv hidden after successful request, picture populated.

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ let instructionsDiv = document.querySelector('#instructionsdiv')
 let searchAndButtonDiv = document.querySelector('#searchandbuttondiv')
 let glowingButton = document.querySelector('.glowing-btn')
 let inputSearch = document.querySelector('#inputsearch')
+let photoDiv = document.getElementById('photodiv')
 const apiKey = '9fe371286bmsh0ed916646153ee4p1f72d4jsnb6ca301a19c9'
 
 
-
- 
+// function for verifying return data from the response variable.
+function isObject(value) {
+    return value !== null && typeof value === 'object';
+}
 
 
 
@@ -17,22 +20,41 @@ console.log("working")
 //Event listener for click on glowing button.
 glowingButton.addEventListener('click', async () => {
 
+    
     const options = {
         method: 'GET',
-        url: 'https://superhero-search.p.rapidapi.com/api/heroes',
+        url: 'https://superhero-search.p.rapidapi.com/api/',
+        params: {hero: inputSearch.value},
         headers: {
           'X-RapidAPI-Key': '9fe371286bmsh0ed916646153ee4p1f72d4jsnb6ca301a19c9',
           'X-RapidAPI-Host': 'superhero-search.p.rapidapi.com'
         }
-      };
-      
-      try {
-          const response = await axios.request(options);
-          console.log(response.data);
-      } catch (error) {
-          console.error(error);
       }
+      
+    let response = await axios.request(options);
+    console.log(response.data);
+    // console.log(response.data.name)
+    // console.log(response.data.appearance.eyeColor)
 
+
+    //sets display to none upon successful dearch for super.
+    if(isObject(response.data)) {
+         instructionsDiv.style.display = 'none'
+    searchAndButtonDiv.style.display = 'none'
+    } else {
+         alert(`Character ${inputSearch.value} not found`)
+    }
+
+    let imageVariable = response.data.images.md
+    photoDiv.innerHTML = `<img src=${imageVariable}>`
+    
+
+        
+
+
+      
+
+      
 
 
 
